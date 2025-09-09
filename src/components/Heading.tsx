dacf575ceea436346e2a17709156cf9d630ceda4
AndileP3 <mamogobalaleandile@gmail.com>
2025-09-09 20:35:29 +0200

    adds a new logo to the header

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,31 +1,18 @@
 import React from "react";
 import styles from "../styles/Heading.module.css";
+import logo from "../assets/logo.png";
 
 const Heading: React.FC = () => {
   return (
     <header className={styles.header}>
       {/* Logo */}
-      <div className={styles.logo}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="36"
-          height="36"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path d="M12 2L2 7h20L12 2z" />
-          <path d="M2 7v7c0 5 5 9 10 9s10-4 10-9V7" />
-        </svg>
-        <span>Vintage Store</span>
-      </div>
+ <div className={styles.logo}>
+    <img src={logo} alt="Vintage Store Logo" className={styles.logoImage} />
+  </div>
 
       {/* Search */}
       <div className={styles.searchBox}>
-        <input type="text" placeholder="Search products..." />
+        <input type="text" placeholder="Search Items, Brands, & Categories" />
       </div>
 
       {/* Cart */}
