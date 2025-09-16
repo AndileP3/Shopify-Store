1edc58000bd31d792feac14256892c14a10c9e67
AndileP3 <mamogobalaleandile@gmail.com>
2025-09-16 22:39:12 +0200

    new form of the filtering container

--- src/components/Filtering.tsx
+++ src/components/Filtering.tsx
@@ -1,37 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../styles/Filtering.module.css";
 
 const categoryImages: Record<string, string> = {
   All: "https://www.orega.com/hubfs/OregaMarkLane_Reception_WithPeople2-1.jpg",
   Woman: "https://www.orega.com/hubfs/OregaBroadGate_OfficeSpace_WithPeople.jpg",
-  Man: "https://www.orega.com/hs-fs/hubfs/virtual-office-header-3.png?width=350&height=300&name=virtual-office-header-3.png",
-  Shirts: "https://www.orega.com/hs-fs/hubfs/Orega_GracechurchSt_Building.jpg?width=600&height=338&name=Orega_GracechurchSt_Building.jpg",
+  Man: "https://www.orega.com/hs-fs/hubfs/virtual-office-header-3.png?width=1200&height=500",
+  Shirts: "https://www.orega.com/hs-fs/hubfs/Orega_GracechurchSt_Building.jpg?width=1200&height=500",
   Socks: "https://www.orega.com/hubfs/OregaMarkLane_Reception_WithPeople2-1.jpg",
 };
 
 const Filtering: React.FC = () => {
   const [selected, setSelected] = useState<string>("All");
+  const [isFixed, setIsFixed] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 200) {
+        setIsFixed(true);
+      } else {
+        setIsFixed(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <div className={styles.filterWrapper}>
-      {/* Tabs */}
-      <div className={styles.tabs}>
-        {Object.keys(categoryImages).map((category) => (
-          <button
-            key={category}
-            onClick={() => setSelected(category)}
-            className={`${styles.tabButton} ${
-              selected === category ? styles.active : ""
-            }`}
-          >
-            {category}
-          </button>
-        ))}
-      </div>
-
-      {/* Image */}
-      <div className={styles.imageContainer}>
-        <img src={categoryImages[selected]} alt={selected} />
+    <div
+      className={`${styles.filterWrapper} ${isFixed ? styles.fixed : ""}`}
+      style={{ backgroundImage: `url(${categoryImages[selected]})` }}
+    >
+      <div className={styles.overlay}>
+        {/* Tabs */}
+        <div className={styles.tabs}>
+          {Object.keys(categoryImages).map((category) => (
+            <button
+              key={category}
+              onClick={() => setSelected(category)}
+              className={`${styles.tabButton} ${
+                selected === category ? styles.active : ""
+              }`}
+            >
+              {category}
+            </button>
+          ))}
+        </div>
       </div>
     </div>
   );
